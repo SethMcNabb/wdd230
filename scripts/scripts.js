275f51ca4be93db7b6d2706ae85115f04989ab26
SethMcNabb <youremail@byui.edu>
2023-04-22 20:28:34 -0600

    Worked on making assignment portal.

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+/* displaying current year by copywrite*/
+let date = new Date();
+let year = date.getFullYear();
+console.log(year);
+document.getElementById("current-year").innerHTML = `&copy; ${year} .:|:. Seth McNabb .:|:. Utah`;
+
+
+/* displaying last date modified with js */
+let dateLastModified = new Date(document.lastModified);
+document.getElementById("last-modified").innerHTML = `Last Updated: ${dateLastModified}`;
