d149bf13e8daaeed9f86feb617ae8a4ebfaababc
SethMcNabb <youremail@byui.edu>
2023-05-19 00:44:20 -0600

    Used guide to get program working.

--- 05Practice/scripts/scripts.js
+++ 05Practice/scripts/scripts.js
@@ -1,28 +1,29 @@
+const list = document.querySelector('ul');
 const input = document.querySelector('input'); 
 const button = document.querySelector('button');
-const list = document.querySelector('list');
 
 button.addEventListener('click', function() {
     //make sure the input is not blank before doing the following remaining tasks in this list
-    const myList = input.value;
+    const myItem = input.value;
     input.value = '';
     //create an li element
-    const newLiElement = document.createElement('li');
+    const listItem = document.createElement('li');
     //create a delete button
-    const newDeleteButton = document.createElement('button');
+    const listBtn = document.createElement('button');
+    //
     //populate the li elements textContent or innerHTML with the input
-    newLiElement.textContent(input);
+    listItem.innerHTML = myItem;
     //populate the button textContent with an ❌
-    newDeleteButton.textContent('❌');
+    listBtn.textContent = '❌';
     //append the li element with the delete button
-    newLiElement.appendChild(newDeleteButton);
+    listItem.appendChild(listBtn);
     //append the list element with the li element just created and appended with text and the delete button
-    list.appendChild(newLiElement);
-    button.textContent('Delete');
+    list.appendChild(listItem);
+    //
     //add an event listener to the delete button that removes the li element when clicked
-    newDeleteButton.addEventListener('click'), function() {
-        list.removeChild(newLiElement);
-    }
+    listBtn.addEventListener('click', function() {
+        list.removeChild(listItem);
+    });
     //send the focus to the input element
     input.focus();
     //change the input value to nothing or the empty string to clean up the interface for the user
