4e0f588dec7ea344420630b5c6b0efb7d653c5d4
SethMcNabb <youremail@byui.edu>
2023-05-20 17:43:43 -0600

    Corrected lack of responsivness, other errors.

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -5,9 +5,10 @@ let dateLastModifiedLong = new Date(document.lastModified);
 let dateLastModified = dateLastModifiedLong.toDateString();
 document.getElementById("footer-info").innerHTML = `&copy ${year} San Diego Chamber | <strong>Seth McNabb</strong> | WDD 230 Project | last Modification: ${dateLastModified}`;
 document.getElementById("header-info").innerHTML = `${dateString}`;
+
+
 const hamButton = document.querySelector('#hamButton');
 const navigation = document.querySelector('.navigation');
-
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('responsive');
 });
