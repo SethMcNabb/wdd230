0b1c6b692ee31f753b400bc36f1c44cc76ffc3c8
SethMcNabb <youremail@byui.edu>
2023-05-26 20:30:28 -0600

    Added alert for no input entries.

--- 05Practice/scripts/scripts.js
+++ 05Practice/scripts/scripts.js
@@ -1,31 +1,24 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input'); 
-const button = document.querySelector('button');
+const list = document.querySelector('ul'); //variable for dom 'ul' element.
+const input = document.querySelector('input'); //variable for user's input.
+const button = document.querySelector('button'); //variable for functional button.
 
-button.addEventListener('click', function() {
-    //make sure the input is not blank before doing the following remaining tasks in this list
+button.addEventListener('click', function() { //function to allow user to add, view, and delete data. 
     const myItem = input.value;
     input.value = '';
-    //create an li element
+    if (myItem === '') {
+        alert("Please enter a favorite BofM chapter")
+    } else {
     const listItem = document.createElement('li');
-    //create a delete button
     const listBtn = document.createElement('button');
-    //
-    //populate the li elements textContent or innerHTML with the input
+    
     listItem.innerHTML = myItem;
-    //populate the button textContent with an ❌
+    
     listBtn.textContent = '❌';
-    //append the li element with the delete button
     listItem.appendChild(listBtn);
-    //append the list element with the li element just created and appended with text and the delete button
     list.appendChild(listItem);
-    //
-    //add an event listener to the delete button that removes the li element when clicked
     listBtn.addEventListener('click', function() {
         list.removeChild(listItem);
     });
-    //send the focus to the input element
+    }
     input.focus();
-    //change the input value to nothing or the empty string to clean up the interface for the user
-
 });
