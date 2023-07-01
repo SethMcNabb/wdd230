a957d7518ec879cac347b7984a7ef41515e11653
SethMcNabb <youremail@byui.edu>
2023-06-30 20:29:40 -0600

    Getting caught up.

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -11,6 +11,7 @@ document.getElementById("header-info").innerHTML = `${dateString}`;
 
 const hamButton = document.querySelector('#hamButton');
 const navigation = document.querySelector('.navigation');
+
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('responsive');
 });
@@ -20,3 +21,22 @@ const dayOfWeek = date.getDay();
 if (dayOfWeek ===1 || dayOfWeek ===2) {
 	document.getElementById("banner").style.display='block' 
 };
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
---
+++ lesson09/scripts/scripts.js
@@ -0,0 +1,38 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+async function getProphetData() {
+	const response = await fetch(url);
+	const data = await response.json();
+	displayProphets(data.prophets);
+}
+  
+getProphetData();
+
+const displayProphets = (prophets) => {
+	const cards = document.querySelector('div.cards'); 
+  
+	prophets.forEach((prophet) => {
+		let card = document.createElement('section');
+		let h2 = document.createElement('h2');
+		let portrait = document.createElement('img');
+		let dateOfBirth = document.createElement('h3');
+		let placeOfBirth = document.createElement('h3');	
+
+		h2.textContent = `${prophet.name} ${prophet.lastname}`;
+		dateOfBirth.textContent = `Date of Birth: ${prophet.dateOfBirth}`;
+		placeOfBirth.textContent = `Place of Birth: ${prophet.placeOfBirth}`;
+
+		portrait.setAttribute('src', prophet.imageurl);
+		portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+		portrait.setAttribute('loading', 'lazy');
+		portrait.setAttribute('width', '340');
+		portrait.setAttribute('height', '440');
+	
+		card.appendChild(h2);
+		card.appendChild(dateOfBirth);
+		card.appendChild(placeOfBirth);
+		card.appendChild(portrait);
+	
+		cards.appendChild(card);
+	}); 
+}
